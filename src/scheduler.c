32fd2bf430b2cf9bf6d09754def94a84f6982049
prabhsimraan <prabhsimraan@gmail.com>
2023-11-20 22:41:52 -0800

    updated scheduler.c

--- src/scheduler.c
+++ src/scheduler.c
@@ -20,11 +20,19 @@
 // Process Queues
 queue_t run_queue;
 
+// Assuming proc_table is a global array of proc_t
+extern proc_t proc_table[PROC_MAX];
+
+
 /**
  * Scheduler timer callback
  */
 void scheduler_timer(void) {
     // Update the active process' run time and CPU time
+    if (active_proc != NULL) {
+        active_proc->run_time++;
+        active_proc->cpu_time++;
+    }
 }
 
 /**
@@ -33,25 +41,58 @@ void scheduler_timer(void) {
  */
 void scheduler_run(void) {
     // Ensure that processes not in the active state aren't still scheduled
+    if (active_proc != NULL && active_proc->state != ACTIVE) {
+        active_proc = NULL;
+    }
 
     // Check if we have an active process
+    if (active_proc != NULL) {
+
         // Check if the current process has exceeded it's time slice
-            // Reset the active time
+        if (active_proc->cpu_time >= SCHEDULER_TIMESLICE) {
+
+        // Reset the active time
+            active_proc->cpu_time = 0;
 
             // If the process is not the idle task, add it back to the scheduler
             // Otherwise, simply set the state to IDLE
+            if (active_proc->pid != 0) {
+                scheduler_add(active_proc);
+            } else {
+                active_proc->state = IDLE;
+            }
 
             // Unschedule the active process
+            active_proc = NULL;
+            }  else {
+            // Ensure that the process state is set
+            active_proc->state = ACTIVE;
+
+        }
+    }
 
     // Check if we have a process scheduled or not
-        // Get the proces id from the run queue
-            // default to process id 0 (idle task) if a process can't be scheduled
+    if (active_proc == NULL) {
+
+    // Get the proces id from the run queue
+    int next_pid = queue_init(&run_queue);
+
+    // default to process id 0 (idle task) if a process can't be scheduled
+    active_proc = (next_pid != -1) ? &proc_table[next_pid] : &proc_table[0];
 
         // Update the active proc pointer
+        active_proc->state = ACTIVE;
+    }
+
 
     // Make sure we have a valid process at this point
+    if (active_proc == NULL) {
+        // Handle the error, log, or take appropriate action
+    }
 
     // Ensure that the process state is set
+    active_proc->state = ACTIVE;
+
 }
 
 /**
@@ -60,7 +101,10 @@ void scheduler_run(void) {
  */
 void scheduler_add(proc_t *proc) {
     // Add the process to the run queue
+    queue_in(&run_queue, proc->pid);
+
     // Set the process state
+    proc->state = IDLE;
 }
 
 /**
@@ -69,11 +113,26 @@ void scheduler_add(proc_t *proc) {
  */
 void scheduler_remove(proc_t *proc) {
     // Iterate through each the process queue
+   int next_pid;
+
+    if (queue_out(&run_queue, &next_pid) == -1) {
+    // Handle the case where the queue is empty
+    } else {
+        if (next_pid == proc->pid) {
     // If the processis found, skip it; otherwise, ensure that each other process remains in the queue
+        }    else {
+        // Otherwise, ensure that each other process remains in the queue
+        queue_in(&run_queue, next_pid);
+    }
+    }
 
     // If the process is the active process, ensure that the active process is cleared so when the
+    if (active_proc != NULL && active_proc->pid == proc->pid) {
+        active_proc = NULL;
+    }
     // scheduler runs again, it will select a new process to run
-}
+    proc->state = PROC_TYPE_NONE;
+ }
 
 /**
  * Initializes the scheduler, data structures, etc.
@@ -82,6 +141,9 @@ void scheduler_init(void) {
     kernel_log_info("Initializing scheduler");
 
     // Initialize any data structures or variables
+    queue_init(&run_queue);
+
     // Register the timer callback (scheduler_timer) to run every tick
+    timer_callback_register(scheduler_timer, SCHEDULER_TIMESLICE, 1);
 }
 
